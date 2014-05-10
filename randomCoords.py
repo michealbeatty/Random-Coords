9fcc622e040cd970a36d0de3b008a90e720acae8
Micheal Beatty <mike@mwbeatty.org>
2014-05-10 13:36:40 -0500

    Change to geopy

--- randomCoords.py
+++ randomCoords.py
@@ -22,35 +22,38 @@ geolocator = GoogleV3()
 fullstring = '{0}, {1}, "{2}", \n'
 
 
-def coordinate_generator(number_of_points, output_file):
+def coordinate_generator(number_of_points):
     counter = 0
     while counter < number_of_points:
         latlng = round(random.uniform(southernmost, northernmost), 6), round(random.uniform(easternmost, westernmost), 6)
-        g = geocoder.reverse(latlng)
-        if g.country != 'United States':
+        try:
+            address, (lat, lng) = geolocator.reverse(latlng, exactly_one=True)
+            if 'USA' not in address:
+                continue
+            else:
+                counter += 1
+                print(lng, lat, address)
+
+        except TypeError:
             continue
-        else:
-            counter += 1
-            output_file.write(fullstring.format(g.x, g.y, g.address))
-            #print counter, fullstring.format(g.x, g.y, g.address)
-    print 'Finished generating %d coordinate points' % counter
 
+coordinate_generator(10)
 
-fname = raw_input("Filename: ")
-if os.path.isfile(fname):
-    proceed_prompt = raw_input("File exists, proceed(y or n)? ")
-    if proceed_prompt.lower() == 'y':
-        fout = open(fname, 'a')
-        number_of_points = int(raw_input("Number of points to generate: "))
-        coordinate_generator(number_of_points, fout)
-        fout.close()
-    else:
-        print 'Aborting . . .'
-        exit()
-else:
-    fout = open(fname, 'a')
-    number_of_points = int(raw_input("Number of points to generate: "))
-    coordinate_generator(number_of_points, fout)
-    fout.close()
+# fname = raw_input("Filename: ")
+# if os.path.isfile(fname):
+#     proceed_prompt = raw_input("File exists, proceed(y or n)? ")
+#     if proceed_prompt.lower() == 'y':
+#         fout = open(fname, 'a')
+#         number_of_points = int(raw_input("Number of points to generate: "))
+#         coordinate_generator(number_of_points, fout)
+#         fout.close()
+#     else:
+#         print 'Aborting . . .'
+#         exit()
+# else:
+#     fout = open(fname, 'a')
+#     number_of_points = int(raw_input("Number of points to generate: "))
+#     coordinate_generator(number_of_points, fout)
+#     fout.close()
 
 #TODO Check for existence of random_addresses.csv and warn before appending.
