b47e4d15771832bdb11b7e8bda6562321ed1ef02
Micheal Beatty <mike@mwbeatty.org>
2015-01-15 17:15:31 -0600

    Refactor

--- randomCoords.py
+++ randomCoords.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
 
 """US Geographic Information
 
@@ -10,54 +10,60 @@ Geographic Center - (39.833333. -98.583333)
 """
 import random
 import os
-from geopy.geocoders import GoogleV3
-#import geocoder
+import geocoder
 
 
-northernmost = 49.
-southernmost = 25.
-easternmost = -66.
-westernmost = -124.
-geolocator = GoogleV3()
+#TODO create function to handle processing of generated coordinates
 
-fullstring = '{0}, {1}, "{2}", \n'
+def coordinate_generator(number_of_points):
+    northernmost = 49.
+    southernmost = 25.
+    easternmost = -66.
+    westernmost = -124.
+
+    coordinate_list = []
 
 
-def coordinate_generator(points, output_file):
-    """ Generate random coordinates
+    fullstring = '{0}, {1}, "{2}", \n'
+    #TODO refactor for separation of concerns This function should only generate coords
 
-    number_of_points - number of random points to generate
-    output_file - file to which the random points will be written
-    """
     counter = 0
-    while counter < points:
-        #This generates a random coordinate based on the most extreme
-        #points in the contiguous United States
-        latlng = round(random.uniform(southernmost, northernmost), 6), round(random.uniform(easternmost, westernmost), 6)
-        try:
-            #This reverse geocodes the random coordinate
-            address, (lat, lng) = geolocator.reverse(latlng, exactly_one=True)
-            if 'USA' not in address:  # We only want USA addresses
-                continue
-            else:
-                counter += 1
-                output_file.write(fullstring.format(lng, lat, address))
-        except TypeError:  # This checks for geocoding that returns NoneType
-            continue
 
-fname = input("Filename: ")
-if os.path.isfile(fname):
-    proceed_prompt = input("File exists, proceed(y or n)? ")
-    if proceed_prompt.lower() == 'y':
+    while counter < number_of_points:
+        latlng = round(random.uniform(southernmost, northernmost), 6), round(random.uniform(easternmost, westernmost), 6)
+        g = geocoder.reverse(latlng)
+        if g.country != 'US':
+            continue
+        else:
+            counter += 1
+            coordinate_list.append(g.x, g.y, g.address)
+            # output_file.write(fullstring.format(g.x, g.y, g.address))
+
+    return coordinate_list
+    print 'Finished generating %d coordinate points' % counter
+
+
+def main():
+    #TODO: main function should handle the actual writing to file
+    fname = raw_input("Filename: ")
+    if os.path.isfile(fname):
+        proceed_prompt = raw_input("File exists, proceed(y or n)? ")
+        if proceed_prompt.lower() == 'y':
+            fout = open(fname, 'a')
+            number_of_points = int(raw_input("Number of points to generate: "))
+            coordinate_generator(number_of_points, fout)
+            fout.close()
+        else:
+            print 'Aborting . . .'
+            exit()
+    else:
         fout = open(fname, 'a')
-        number_of_points = int(input("Number of points to generate: "))
+        number_of_points = int(raw_input("Number of points to generate: "))
         coordinate_generator(number_of_points, fout)
         fout.close()
-    else:
-        print('Aborting . . .')
-        exit()
-else:
-    fout = open(fname, 'a')
-    number_of_points = int(input("Number of points to generate: "))
-    coordinate_generator(number_of_points, fout)
-    fout.close()
+
+
+#TODO Add command line arguments for filename and number of locations
+
+if __name__ == '__main__':
+    main()
