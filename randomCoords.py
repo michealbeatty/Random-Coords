ccdc09d1db00154f424627af76a75bc16de078cc
Micheal Beatty <mike@mwbeatty.org>
2015-01-15 21:13:58 -0600

    Refactor to add command line arguments and move file write out of coordinate generator into main.

--- randomCoords.py
+++ randomCoords.py
@@ -8,62 +8,71 @@ Easternmost - (44.815278. -65.949722)
 Westernmost - (48.164167. -124.733056)
 Geographic Center - (39.833333. -98.583333)
 """
-import random
+import argparse
 import os
+import random
+
 import geocoder
 
 
 #TODO create function to handle processing of generated coordinates
 
 def coordinate_generator(number_of_points):
-    northernmost = 49.
-    southernmost = 25.
-    easternmost = -66.
-    westernmost = -124.
+    NORTHERNMOST = 49.
+    SOUTHERNMOST = 25.
+    EASTERNMOST = -66.
+    WESTERNMOST = -124.
 
     coordinate_list = []
 
-
-    fullstring = '{0}, {1}, "{2}", \n'
     #TODO refactor for separation of concerns This function should only generate coords
 
     counter = 0
 
     while counter < number_of_points:
-        latlng = round(random.uniform(southernmost, northernmost), 6), round(random.uniform(easternmost, westernmost), 6)
+        latlng = round(random.uniform(SOUTHERNMOST, NORTHERNMOST), 6), round(random.uniform(EASTERNMOST, WESTERNMOST), 6)
         g = geocoder.reverse(latlng)
         if g.country != 'US':
             continue
         else:
             counter += 1
-            coordinate_list.append(g.x, g.y, g.address)
+            coordinate_list.append((g.x, g.y, g.address))
             # output_file.write(fullstring.format(g.x, g.y, g.address))
-
-    return coordinate_list
     print 'Finished generating %d coordinate points' % counter
+    return coordinate_list
 
 
-def main():
+
+def main(points, fname):
     #TODO: main function should handle the actual writing to file
-    fname = raw_input("Filename: ")
+    fullstring = '{0}, {1}, "{2}", \n'
+    coordinates = []
     if os.path.isfile(fname):
         proceed_prompt = raw_input("File exists, proceed(y or n)? ")
         if proceed_prompt.lower() == 'y':
             fout = open(fname, 'a')
-            number_of_points = int(raw_input("Number of points to generate: "))
-            coordinate_generator(number_of_points, fout)
-            fout.close()
+            number_of_points = points
+            coordinates = coordinate_generator(number_of_points)
+            # fout.close()
+            for loc in coordinates:
+                fout.write(fullstring.format(loc[0], loc[1], loc[2]))
         else:
             print 'Aborting . . .'
             exit()
     else:
         fout = open(fname, 'a')
-        number_of_points = int(raw_input("Number of points to generate: "))
-        coordinate_generator(number_of_points, fout)
+        number_of_points = points
+        coordinates = coordinate_generator(number_of_points)
+        for loc in coordinates:
+            fout.write(fullstring.format(loc[0], loc[1], loc[2]))
         fout.close()
 
 
 #TODO Add command line arguments for filename and number of locations
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("points", type=int, help="number of points to generate")
+    parser.add_argument("fname", help="name of output file")
+    args = parser.parse_args()
+    main(args.points, args.fname)
